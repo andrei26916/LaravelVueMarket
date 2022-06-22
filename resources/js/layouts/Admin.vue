0c9216d80ae9a1b12ba702b8565cd8bf12afc674
andrei26916 <andrey.stosh@yandex.ru>
2022-06-22 13:04:36 +0800

    (+) middleware admin panel

--- resources/js/layouts/Admin.vue
+++ resources/js/layouts/Admin.vue
@@ -1,23 +1,48 @@
 <template>
-    <div class="admin_div_flex">
+    <div class="admin_div_flex" v-if="authorisation()">
         <MenuComponent/>
         <div class="content_admin">
             <BreadcrumbComponent/>
             <slot/>
         </div>
     </div>
+    <div v-else>
+        <Auth></Auth>
+    </div>
 </template>
 
 <script>
 import MenuComponent from '../components/Admin/MenuComponent';
 import BreadcrumbComponent from '../components/Admin/BreadcrumbComponent';
+import Auth from '../page/Auth';
 import {mapGetters} from "vuex";
 
 export default {
     name: "Admin",
     components: {
-      MenuComponent, BreadcrumbComponent
+      MenuComponent, BreadcrumbComponent, Auth
     },
+    methods: {
+        ...mapGetters(['getUser']),
+        authorisation(){
+            let check = false;
+            if (this.getUser()) {
+                if (Object.keys(this.getUser()).length > 0){
+                    if (this.getUser().roles){
+                        this.getUser().roles.forEach(element => {
+                            if (element.slug === 'admin'){
+                                check = true;
+                            }
+                        })
+                    }
+                }
+            }
+            return check;
+        },
+        redirect() {
+            return this.$router.push({name: 'home'})
+        }
+    }
 }
 </script>
 
